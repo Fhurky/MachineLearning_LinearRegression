bba1d3d7b37a4bf4c2538414f9c806f27510b199
Fhurky <furkocal@gmail.com>
2024-08-25 16:47:48 +0300

    Updating plots

--- LinearModel/LinearModel.py
+++ LinearModel/LinearModel.py
@@ -6,15 +6,15 @@ Created on Sun Aug 25 15:38:10 2024
 """
 
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 import pandas as pd
 
 data = pd.read_csv("Student_Performance.csv")
 data = data.drop(['Extracurricular Activities'], axis = 1)
 
-train, test = train_test_split(data, test_size=0.4, random_state=42)
+train, test = train_test_split(data, test_size=0.2, random_state=42)
 
 train_results = train.iloc[:,-1]
 test_results = test.iloc[:,-1]
@@ -26,6 +26,15 @@ lm = LinearRegression()
 lm.fit(train,train_results)
 
 prediction = lm.predict(test)
-
-
 print("R2-score: %.2f" % r2_score(test_results, prediction))
+
+
+plt.figure(figsize=(10, 10))
+plt.scatter(prediction, test_results, color='blue')
+plt.title('Student Score vs. Study Hours')
+plt.xlabel('Study Hours')
+plt.ylabel('Performance Index')
+plt.grid(True)
+plt.show()
+
+
